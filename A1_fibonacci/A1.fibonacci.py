d9063395e5f3c35714b13d54c03d6f0d443fcec1
CodeGeeks-tech <freelance.codegeeks@gmail.com>
2025-11-13 05:09:58 +0530

    Add files via upload

---
+++ A1_fibonacci/A1.fibonacci.py
@@ -0,0 +1,56 @@
+def recursive_fibonacci(n): 
+    if n <= 1: 
+        return n 
+    else: 
+        return recursive_fibonacci(n-1) + recursive_fibonacci(n-2) 
+ 
+def non_recursive_fibonacci(n): 
+    first = 0 
+    second = 1 
+    print(first)
+    if n > 1:
+        print(second)
+    for i in range(2, n):
+        third = first + second 
+        print(third)
+        first, second = second, third 
+ 
+if __name__ == "__main__": 
+    n = int(input("Enter how many terms you want: "))
+
+    print("\nFibonacci Series using Recursive Method:")
+    for i in range(n): 
+        print(recursive_fibonacci(i))
+    
+    print("\nFibonacci Series using Non-Recursive (Iterative) Method:")
+    non_recursive_fibonacci(n)
+
+
+"""
+================ SAMPLE INPUT =================
+
+Enter how many terms you want: 7
+
+
+================ SAMPLE OUTPUT ================
+
+Fibonacci Series using Recursive Method:
+0
+1
+1
+2
+3
+5
+8
+
+Fibonacci Series using Non-Recursive (Iterative) Method:
+0
+1
+1
+2
+3
+5
+8
+
+================================================
+"""
